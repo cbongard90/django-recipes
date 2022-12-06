fb9881b7301efb691f9750442f6a397efcab185a
keshny <keshnyseetahul@gmail.com>
2022-12-06 10:27:42 +0400

    added recipe and ingredient models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    instructions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    quantity = models.CharField(max_length=50) 
+    unit = models.CharField(max_length=50) 
